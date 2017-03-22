52711f001e09bb0e966f4d95064ed944869da1a7
Adnela <adneela@hotmail.com>
2017-03-22 21:15:58 +0100

    Prosireni testovi

--- spec/klix_spec.rb
+++ spec/klix_spec.rb
@@ -21,21 +21,52 @@ RSpec.describe "Klix test" do
 	       @browser.wait_until{@browser.div(:class=>"vremenska hidden-md hidden-sm hidden-xs").exists?}
 	       @browser.link(:class=>"dan").click
 	       @browser.wait_until{@browser.div(:class=>"col-sm-9 col-xs-7").exists?}
-	       expect(@browser.div(:class=>"kategorizacija").link(:href=>"http://www.klix.ba/najnovije").text).to eql("Najnovije")
-		    
-
-	    
-		   
-			
+	       expect(@browser.div(:class=>"kategorizacija").link(:href=>"http://www.klix.ba/najnovije").text).to eql("Najnovije")	
 		
 		end
 
    end
 
+ 
+
+   context "Click on logo" do
+
+   	it "Naslovnica" do
+
+   		@browser.span(:class=>"icon_kx_logo").click
+   		@browser.wait_until{@browser.div(:class=>"block-head main hidden-xs").exists?}
+   		expect(@browser.text).to include ("Vijesti")
+   		
+   	end
+
+   end
+
+  context "Click on Vijesti" do
+
+   	it "Vijesti" do
+
+   		@browser.ul(:class=>"categories").li(:class=>"li_vijesti").click
+   		@browser.wait_until{@browser.div(:class=>"breadcrumbs bg_vijesti").exists?}
+   		expect(@browser.text).to include ("Vijesti")
+
+   
+   	end
+   
+   end
+
+   context "Click on Biznis" do
+
+   	it "Biznis" do
+
+   		@browser.ul(:class=>"categories").li(:class=>"li_biznis").click
+   		@browser.wait_until{@browser.div(:class=>"breadcrumbs bg_biznis").exists?}
+   		expect(@browser.text).to include ("Biznis")
+
+   	end
+   end
+
 
    context "Close browser" do
-
-	  
 
 	   it "Browser close" do
 
--- spec/sportsport_spec.rb
+++ spec/sportsport_spec.rb
@@ -14,7 +14,17 @@ RSpec.describe "SportSport test" do
 
 
 
-	 
+	context "Click on News" do
+
+    	it "News details" do
+
+   		  @browser.article(:class=>"position_one").click
+   		  expect(@browser.div(:id=>"news_page").exists?)
+     	
+     	end
+
+   end
+
     context "Click on TABELE " do
 	
         it "New Page Tabele" do
@@ -28,6 +38,39 @@ RSpec.describe "SportSport test" do
 		end
 
    end
+
+   context "Click on Premijer liga BiH" do
+
+   	it "Table" do
+
+   		@browser.link(:text=>"Premijer Liga BiH").click
+   		expect(@browser.table(:class=>" standings_league standings").exists?)
+   	end
+   
+   end
+
+   context "Click on radio button Raspored utakmica" do
+
+   	it "Raspored utakmica" do
+
+   		@browser.input(:value=>"2").click
+   		expect(@browser.div(:id=>"tableOuterWrapper").exists?)
+   	end
+
+   end
+
+   context "Click on Trening" do
+
+   	it "Trening" do
+
+   		@browser.link(:class=>"recreation").click
+   		expect(@browser.div(:class=>"recreation_top_container").exists?)
+
+   	end
+   
+   end
+
+
 
 
 
@@ -45,4 +88,4 @@ RSpec.describe "SportSport test" do
 
 
 
-end 
+end
